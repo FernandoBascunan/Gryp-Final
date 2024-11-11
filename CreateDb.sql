69681435708df060e66eaf575b816a10c19acc51
Fernando <fernandobaspe@gmail.com>
2024-11-10 23:55:19 -0300

    RegistroDB

--- CreateDb.sql
+++ CreateDb.sql
@@ -1,14 +1,16 @@
 CREATE TABLE users (
-    userID INT PRIMARY KEY,
+    userID INT AUTO_INCREMENT PRIMARY KEY,
     userName VARCHAR(50),
+    rut VARCHAR(50),
+    email VARCHAR(50),
+    phone VARCHAR(50),
     region VARCHAR(50),
     commune VARCHAR(50),
-    Phone VARCHAR(20),
-    rut VARCHAR(9)
+    password VARCHAR(50)
 );
 
 CREATE TABLE waiter (
-    waiterID INT PRIMARY KEY,
+    waiterID INT AUTO_INCREMENT PRIMARY KEY,
     waiterName VARCHAR(50),
     wRut VARCHAR(9),
     wPhone VARCHAR(20)
@@ -18,25 +20,25 @@ CREATE TABLE workers (
     userID INT,
     waiterID INT,
     PRIMARY KEY (userID, waiterID),
-    FOREIGN KEY (userID) REFERENCES users(userID),
-    FOREIGN KEY (waiterID) REFERENCES waiter(waiterID)
+    FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE,
+    FOREIGN KEY (waiterID) REFERENCES waiter(waiterID) ON DELETE CASCADE
 );
 
 CREATE TABLE tables (
-    tableID INT PRIMARY KEY,
+    tableID INT AUTO_INCREMENT PRIMARY KEY,
     tableStatus BOOLEAN
 );
 
 CREATE TABLE orders (
-    orderID INT PRIMARY KEY,
+    orderID INT AUTO_INCREMENT PRIMARY KEY,
     waiterID INT,
     tableID INT,
-    FOREIGN KEY (waiterID) REFERENCES waiter(waiterID),
-    FOREIGN KEY (tableID) REFERENCES tables(tableID)
+    FOREIGN KEY (waiterID) REFERENCES waiter(waiterID) ON DELETE SET NULL,
+    FOREIGN KEY (tableID) REFERENCES tables(tableID) ON DELETE SET NULL
 );
 
 CREATE TABLE menu (
-    menuID INT PRIMARY KEY,
+    menuID INT AUTO_INCREMENT PRIMARY KEY,
     dishName VARCHAR(50),
     dishStatus BOOLEAN
 );
@@ -45,12 +47,12 @@ CREATE TABLE orderContent (
     orderID INT,
     menuID INT,
     PRIMARY KEY (orderID, menuID),
-    FOREIGN KEY (orderID) REFERENCES orders(orderID),
-    FOREIGN KEY (menuID) REFERENCES menu(menuID)
+    FOREIGN KEY (orderID) REFERENCES orders(orderID) ON DELETE CASCADE,
+    FOREIGN KEY (menuID) REFERENCES menu(menuID) ON DELETE CASCADE
 );
 
 CREATE TABLE storage (
-    productID INT PRIMARY KEY,
+    productID INT AUTO_INCREMENT PRIMARY KEY,
     productType VARCHAR(50),
     loc VARCHAR(50),
     productName VARCHAR(50),
@@ -62,6 +64,6 @@ CREATE TABLE recipe (
     menuID INT,
     productID INT,
     PRIMARY KEY (menuID, productID),
-    FOREIGN KEY (menuID) REFERENCES menu(menuID),
-    FOREIGN KEY (productID) REFERENCES storage(productID)
+    FOREIGN KEY (menuID) REFERENCES menu(menuID) ON DELETE CASCADE,
+    FOREIGN KEY (productID) REFERENCES storage(productID) ON DELETE CASCADE
 );
